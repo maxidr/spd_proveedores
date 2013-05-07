deb27a20c9d026eb78f08f01daa8fc76ac37be29
Maximiliano Dello Russo <maxidr@gmail.com>
2013-05-07 17:36:47 -0300

    Se añade información del init

--- init.rb
+++ init.rb
@@ -1,12 +1,13 @@
+# encoding: UTF-8
 require_dependency 'layout_base_hook'
 
 Redmine::Plugin.register :spd_proveedores do
   name 'Spd Proveedores plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Maximiliano Dello Russo'
+  description 'Gestión de proveedores para la plataforma SPD'
+  version '0.1.0'
+  url 'https://github.com/maxidr/spd_proveedores'
+  author_url 'https://coderwall.com/maxidr'
 
   #permission :spd_proveedores,  { :project_providers => :index }, public: true
   menu :project_menu, :providers, { :controller => 'project_providers', :action => 'index' }, caption: 'Proveedores', param: :project_id
